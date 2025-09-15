06c3efdfa564b14fbcbedb21e5fa2bc7d2ad5bc1
inathan44 <inathan44@yahoo.com>
2025-09-15 11:29:49 -0700

    Fix build issue

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-
-import Map from '@/components/Map';
+import dynamic from 'next/dynamic';
+const Map = dynamic(() => import('@/components/Map'), { ssr: false });
 import Link from 'next/link';
 
 export default function ExamplePage() {
